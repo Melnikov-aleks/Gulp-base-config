e640e57bbef61b2da3971b554013b05d40547caa
Alexander <melnikov38.alex@gmail.com>
2021-01-06 16:58:27 +0400

    fix tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 const { src, dest, series, parallel, watch } = require('gulp');
 const del = require('del');
 const plumber = require('gulp-plumber');
+const gulpIf = require('gulp-if');
 
 const sourcemaps = require('gulp-sourcemaps');
 const concat = require('gulp-concat');
+const flatten = require('gulp-flatten');
 
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCss = require('gulp-clean-css');
+const tildeImporter = require('node-sass-tilde-importer');
 
 const htmlmin = require('gulp-htmlmin');
 
@@ -27,26 +30,32 @@ function html() {
     return src('src/**/*.html')
         .pipe(plumber())
         .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulpIf(checkName, flatten()))
         .pipe(dest('dist'));
 }
 
 function styles() {
-    return src('src/**/*.s?ss')
+    return src('src/**/*.s{a,c}ss')
         .pipe(plumber())
         .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(concat('main.min.css'))
+        .pipe(
+            sass({
+                importer: tildeImporter,
+            })
+        )
+        .pipe(concat('styles.min.css'))
         .pipe(autoprefixer())
         .pipe(cleanCss())
         .pipe(sourcemaps.write())
-        .pipe(dest('dist/'));
+        .pipe(dest('dist/'))
+        .pipe(sync.stream());
 }
 
 function scripts() {
     return src('src/**/*.js')
         .pipe(plumber())
         .pipe(sourcemaps.init())
-        .pipe(concat('index.min.js'))
+        .pipe(concat('scripts.min.js'))
         .pipe(
             babel({
                 presets: ['@babel/env'],
@@ -58,11 +67,15 @@ function scripts() {
 }
 
 function images() {
-    return src('src/img/**/*.(png|jpg|svg|gif)')
+    return src('src/img/**/*.{png,jpg,svg,gif}')
         .pipe(plumber())
         .pipe(newer('dist/images/'))
         .pipe(imagemin())
         .pipe(dest('dist/images/'));
+}
+
+function moveFavicons() {
+    return src('src/favicons/*').pipe(dest('dist/'));
 }
 
 function delImg() {
@@ -81,13 +94,23 @@ function serve(cb) {
     cb();
 }
 
+function reload(cb) {
+    sync.reload();
+    cb();
+}
+
 function watching() {
-    watch('src/**/*.html', series(html)).on('change', sync.reload);
-    watch('src/**/*.s?ss', series(styles)).on('change', sync.reload);
-    watch('src/**/*.js', series(scripts)).on('change', sync.reload);
-    watch('src/img/**/*.(png|jpg|svg|gif)', series(images)).on('change', sync.reload);
+    watch('src/**/*.html', series(html, reload));
+    watch('src/**/*.s{a,c}ss', series(styles));
+    watch('src/**/*.js', series(scripts, reload));
+    watch('src/img/**/*.{png,jpg,svg,gif}', series(images, reload));
 }
 
 exports.clear = clear;
-exports.delImg = delImg;
-exports.default = series(clear, parallel(html, styles, scripts, images), serve, watching);
+exports.img = series(delImg, images);
+exports.default = series(
+    clear,
+    parallel(html, styles, scripts, images, moveFavicons),
+    serve,
+    watching
+);
